9a2fd06bfb4517980a43dd5be81b874773db199c
weeklyd3 <timothywei.spam@gmail.com>
2021-05-29 03:43:20 -0700

    Deleted redundant numbers/ folder
    The numbers/ folder was an exact copy. This commit, with a parent of d1748deb6752ed9b7bfad22c6467dc7306a66f9a, removed this folder.

--- numbers/level1/enter.php
+++
@@ -1,7 +0,0 @@
-<h1>Level 1 (Practice Round!)</h1>
-<form action="correct.php" method="post">
-<table>
-<tr><td>Problem:</td><td>Enter the answer here:</td></tr>
-<tr><td>Enter 6+3:</td><td><input type="text" placeholder="Problem 1" name="1st" /></td></tr><tr><td>Enter 5+4:</td><td><input type="text" placeholder="Problem 2" name="2nd" /></td></tr><tr><td>Enter 4+9:</td><td><input type="text" placeholder="Problem 3" name="3rd" /></td></tr></td></tr><tr><td>Enter 6+3:</td><td><input type="text" placeholder="Problem 4" name="4th" /></td></tr><tr><td>Enter 1+2:</td><td><input type="text" placeholder="Problem 5" name="5th" /></td></tr>
-<tr><td>
-<input type="reset" value="< Back" onclick="history.back(1);" /></td><td><input type="submit" value="Next >" /></td></tr></table></form>
